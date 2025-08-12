10b1f228565e7de18d139f61ce7cad9fa2b1ab3c
CodeForgeNet <codeforgenet@icloud.com>
2025-08-12 19:58:58 +0530

    initial commit

--- backend-project/src/main/java/com/cfn/main/controllers/HomeController.java
+++ backend-project/src/main/java/com/cfn/main/controllers/HomeController.java
@@ -1,19 +1,33 @@
 package com.cfn.main.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+	
+	
+	private Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 
 	@GetMapping
-	public String home() {
+	public String home(
+			@RequestParam(defaultValue = "default value", required=false) String value
+			) {
+//		System.out.println("api calling : home");	//Not recomended to use
 		
-		System.out.println("api calling : home");
+		logger.info("This is api calling {}", value);
+		logger.debug("This is api calling {}", value);
+		logger.error("This is api calling {}", value);
+		logger.trace("This is api calling {}", value);
+		logger.warn("This is api calling {}", value);
 		return "This is home api";
 	}
 	
